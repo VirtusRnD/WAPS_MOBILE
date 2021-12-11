23f5b8a050f3ec204a3106fb76b1304c4bed0e71
MehmetKadriGofralilar <mehmetgofralilarformal@hotmail.com>
2021-12-11 03:51:33 +0300

    Almost Done

--- app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/ArticlesPageActivity.java
+++ app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/ArticlesPageActivity.java
@@ -3,6 +3,7 @@ package WildfireAnalysisAndPredictionSystem.test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
