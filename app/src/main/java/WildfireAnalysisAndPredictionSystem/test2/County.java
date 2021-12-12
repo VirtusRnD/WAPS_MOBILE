1e8b5ac8f269679d006c8e6668663ca7f9a1c0b9
MehmetKadriGofralilar <mehmetgofralilarformal@hotmail.com>
2021-12-12 14:11:29 +0300

    Initial commit

---
+++ app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/County.java
@@ -0,0 +1,27 @@
+package WildfireAnalysisAndPredictionSystem.test2;
+
+public class County {
+    public String countyName;
+    public Boolean isFav;
+
+    public County(String countyName, Boolean isFav) {
+        this.countyName = countyName;
+        this.isFav = isFav;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public Boolean getIsFav() {
+        return isFav;
+    }
+
+    public void setIsFav(Boolean isFav) {
+        this.isFav = isFav;
+    }
+}
