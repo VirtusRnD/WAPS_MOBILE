1c5ae28b6798a31d771cc9e231501508bf63debb
MehmetKadriGofralilar <mehmetgofralilarformal@hotmail.com>
2021-12-28 15:57:39 +0300

    "cleaning done"

---
+++ app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/PopDeleteAccount.java
@@ -0,0 +1,38 @@
+package WildfireAnalysisAndPredictionSystem.test2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+public class PopDeleteAccount extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_pop_delete_account);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.25));
+
+        //TODO add checking condition
+
+        Button delete = findViewById(R.id.delete_button);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PopDeleteAccount.this, SignInPageActivity.class);
+                startActivity(intent);
+                //TODO somehow kill settingsPage for the user not being able to sign in by back button
+            }
+        });
+
+    }
+}
