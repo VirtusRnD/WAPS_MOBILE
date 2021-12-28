ffffcf60952b6c0009bf7cc63ec1f52bdb33ed60
MehmetKadriGofralilar <mehmetgofralilarformal@hotmail.com>
2021-12-28 15:33:41 +0300

    "deleteAccount added"

--- app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/SettingsPageActivity.java
+++ app/src/main/java/WildfireAnalysisAndPredictionSystem/test2/SettingsPageActivity.java
@@ -16,37 +16,12 @@ public class SettingsPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
 
-        Button log_out_director = findViewById(R.id.button_log_out);
-        log_out_director.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsPageActivity.this, SignInPageActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        TextView go_to_guide = findViewById(R.id.guide_director);
-        go_to_guide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingsPageActivity.this, GuidePageActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button delete_account = findViewById(R.id.button_delete_account);
-        delete_account.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO ask for user's password, delete account, go to sign in page
-            }
-        });
-
         TextView change_username = findViewById(R.id.settings_username);
         change_username.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SettingsPageActivity.this, PopUsername.class));
+                Intent intent = new Intent(SettingsPageActivity.this, PopUsername.class);
+                startActivity(intent);
             }
         });
 
@@ -54,7 +29,8 @@ public class SettingsPageActivity extends AppCompatActivity {
         change_email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SettingsPageActivity.this, PopEmail.class));
+                Intent intent = new Intent(SettingsPageActivity.this, PopEmail.class);
+                startActivity(intent);
             }
         });
 
@@ -62,7 +38,8 @@ public class SettingsPageActivity extends AppCompatActivity {
         change_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SettingsPageActivity.this, PopPassword.class));
+                Intent intent = new Intent(SettingsPageActivity.this, PopPassword.class);
+                startActivity(intent);
             }
         });
 
@@ -76,12 +53,52 @@ public class SettingsPageActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
+            public void onStartTrackingTouch(SeekBar seekBar) {}
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+
+
+
+        TextView info = findViewById(R.id.settings_info);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsPageActivity.this, AboutUsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        TextView go_to_guide = findViewById(R.id.guide_director);
+        go_to_guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsPageActivity.this, GuidePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button log_out_director = findViewById(R.id.button_log_out);
+        log_out_director.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsPageActivity.this, SignInPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button delete_account = findViewById(R.id.button_delete_account);
+        delete_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO ask for user's password, delete account, go to sign in page
+
+                Intent intent = new Intent(SettingsPageActivity.this, PopDeleteAccount.class);
+                startActivity(intent);
 
             }
         });
